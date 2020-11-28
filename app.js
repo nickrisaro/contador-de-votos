9b018cc911edc1c06e8c6ee680fc398e83a893d9
Nayla Portas <naylaportas@gmail.com>
2020-11-28 15:15:51 -0300

    Utiliza express

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
-const http = require('http');
 const recolectorDeVotos = require("./recolectorDeVotos");
 const TelegramBot = require('node-telegram-bot-api');
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors')
+var app = express();
+
+// Convierte una petición recibida a objeto JSON
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(cors());
 
 const { TELEGRAM_TOKEN } = process.env
-
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
+const recolector = new recolectorDeVotos();
 var requestCount = 1;
 var botRequestCount = 1;
 
-const recolector = new recolectorDeVotos();
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin','*');
-  console.log("Pedidos:" + requestCount++);
-  res.end(JSON.stringify(recolector.votosAcumulados()));
-});
-
-server.on('error', (e) => {
-  console.log('Error en el servidor: ' + e.message);
-})
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, function(){
+  console.log(`Server running in http://localhost:${port}`);
 });
 
 setImmediate(recolector.obtenerVotos);
 setInterval(recolector.obtenerVotos, 300000);
+
+app.get('/', function(req, res){
+	res.status(200).send(recolector.votosAcumulados()); 
+});
 
 // var bot = new TelegramBot(TELEGRAM_TOKEN, {polling: true});
 // bot.getMe().then(function (me) {
